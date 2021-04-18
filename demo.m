5b41141352a3c4a9c64b72067b201d4c47d56be1
XiaoweiGu <xw.gu@hotmail.com>
2021-04-18 16:59:21 +0100

    Update demo.m

--- demo.m
+++ demo.m
@@ -1,14 +1,14 @@
 clear all
 clc
 close all
+%% AFC clusters data chunk-by-chunk to increase its computational efficiency.
 
 load exampledata % load data
-% % 
 
-NumberofChunks=2;  
-GranularityLevel=4;
-seq=randperm(length(data(:,1)));
-chunksize=ceil(length(data(:,1))/NumberofChunks);
+NumberofChunks=2; % number of data chunks
+GranularityLevel=4; % level of granularity
+
+chunksize=ceil(length(data(:,1))/NumberofChunks); % calculate the chunk size based on the number of data samples and the number of chunks.
 
 [Centre,IDX]=AFC(data,GranularityLevel,chunksize);  %% AFC clusters data chunk-by-chunk
 
