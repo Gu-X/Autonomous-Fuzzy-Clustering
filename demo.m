fa1445aeda6b9f9bfca273637c0da4b71bef36ec
XiaoweiGu <xw.gu@hotmail.com>
2021-04-18 16:47:46 +0100

    Update demo.m

--- demo.m
+++ demo.m
@@ -2,14 +2,17 @@ clear all
 clc
 close all
 
-load exampledata
+load exampledata % load data
 % % 
 
-NumberofChunks=2;
+NumberofChunks=2;  
 GranularityLevel=4;
 seq=randperm(length(data(:,1)));
 chunksize=ceil(length(data(:,1))/NumberofChunks);
 
-[Centre,IDX]=AFC(data,GranularityLevel,chunksize);
+[Centre,IDX]=AFC(data,GranularityLevel,chunksize);  %% AFC clusters data chunk-by-chunk
+
+% Centre- cluster centres
+% IDX   - the ID of the cluster each data sample belongs to after defuzzification
 
 
